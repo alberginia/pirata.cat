ce858573716f3d44f994f242fa140df5a4210a04
David Arcos <david.arcos@gmail.com>
2010-08-17 00:22:41 +0200

    Easier local_settings deploy in production

--- pirata_cat/settings.py
+++ pirata_cat/settings.py
@@ -2,7 +2,7 @@
 import os
 gettext = lambda s: s
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'database.sqlite',       # Or path to database file if using sqlite3.
+    	'NAME': '%s/database.sqlite' % DIR,       # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -139,3 +139,9 @@ CMS_TEMPLATES = (
 
 #LOGIN_REDIRECT_URL = "/"
 
+
+try:
+    from local_settings import *
+except:
+    pass
+    
