db4a8b3b30c12139f237d220ca102f7f93ce0e71
Dario Castañé <i@dario.im>
2014-02-25 23:40:51 +0100

    Starting migration from Wordpress to Zas

---
+++ wp-content/themes/twentyeleven/js/rss.js
@@ -0,0 +1,21 @@
+function pad(n, len) {
+    s = n.toString();
+    if (s.length < len) {
+        s = ('0000000000' + s).slice(-len);
+    }
+    return s;   
+}
+
+(function($){var current=null;$.fn.rssfeed=function(url,options){var defaults={limit:10,header:true,titletag:'h4',date:true,content:true,snippet:true,showerror:true,errormsg:'',key:null};var options=$.extend(defaults,options);return this.each(function(i,e){var $e=$(e);if(!$e.hasClass('rssFeed'))$e.addClass('rssFeed');if(url==null)return false;var api="http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&q="+url;if(options.limit!=null)api+="&num="+options.limit;if(options.key!=null)api+="&key="+options.key;$.getJSON(api,function(data){if(data.responseStatus==200){_callback(e,data.responseData.feed,options);}else{if(options.showerror)
+if(options.errormsg!=''){var msg=options.errormsg;}else{var msg=data.responseDetails;};$(e).html('<div class="rssError"><p>'+msg+'</p></div>');};});});};var _callback=function(e,feeds,options){if(!feeds){return false;}
+var html='';var row='odd';if(options.header)
+html+='<div class="rssHeader">'+'<a href="'+feeds.link+'" title="'+feeds.description+'">'+feeds.title+'</a>'+'</div>';html+='<div class="rssBody">'+'<ul>';for(var i=0;i<feeds.entries.length;i++){var entry=feeds.entries[i];var entryDate=new Date(entry.publishedDate);/*var pubDate=entryDate.toLocaleDateString()+' '+entryDate.toLocaleTimeString();*/var pubDate=pad(entryDate.getDate(), 2) + "-" + pad(entryDate.getMonth()+1, 2) + "-" + entryDate.getFullYear() + " a les " + pad(entryDate.getHours(), 2) + ":" + pad(entryDate.getMinutes(), 2);html+='<li class="rssRow '+row+'">'+'<'+options.titletag+'><a href="'+entry.link+'" title="View this feed at '+feeds.title+'">'+entry.title+'</a></'+options.titletag+'>'
+if(options.date)html+='<div>'+pubDate+'</div>'
+if(options.content){if(options.snippet&&entry.contentSnippet!=''){var content=entry.contentSnippet;}else{var content=entry.content;}
+html+='<p>'+content+'</p>'}
+html+='</li>';if(row=='odd'){row='even';}else{row='odd';}}
+html+='</ul>'+'</div>'
+feed=$(html);
+feed.find('embed').attr('width', 425).attr('height', 344);
+feed.find('img').attr('width', 425).attr('height', 344);
+$(e).html(feed.html());};})(jQuery);
